d7986c63e8ed7aab1fa77601307aebdc834692f7
guttacodev <mail@guttaco.com>
2023-04-08 16:53:21 +0200

    V1.1.0 Added Kelp Bowl Dried Kelp Bowl Dried Kelp Bowl with Rice recipes and textures fixed bonemeal and made it so TOP can read the seed state properly
    blue

--- src/main/java/net/guttaco_dev/guttaco_rice/block/custom/RicePlantBlock.java
+++ src/main/java/net/guttaco_dev/guttaco_rice/block/custom/RicePlantBlock.java
@@ -4,15 +4,19 @@ import net.guttaco_dev.guttaco_rice.item.ModItems;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.ItemLike;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.CropBlock;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.IntegerProperty;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
 
 public class RicePlantBlock extends CropBlock {
+//    public static final IntegerProperty AGE = BlockStateProperties.AGE_3;
+    public static final int MAX_AGE = 3;
     public static final IntegerProperty AGE = BlockStateProperties.AGE_3;
     private static final VoxelShape[] SHAPE_BY_AGE = new VoxelShape[]{
             Block.box(0.0D, 0.0D, 0.0D, 16.0D, 2.0D, 16.0D),
@@ -23,6 +27,9 @@ public class RicePlantBlock extends CropBlock {
     public RicePlantBlock(Properties pProperties) {
         super(pProperties);
     }
+
+    @Override
+    public IntegerProperty getAgeProperty() {return AGE;}
 
     @Override
     public int getMaxAge() {
@@ -37,6 +44,13 @@ public class RicePlantBlock extends CropBlock {
     @Override
     public VoxelShape getShape(BlockState pState, BlockGetter pLevel, BlockPos pPos, CollisionContext pContext) {
         return SHAPE_BY_AGE[pState.getValue(this.getAgeProperty())];
+    }
+
+    @Override
+    protected int getBonemealAgeIncrease(Level pLevel) {return super.getBonemealAgeIncrease(pLevel) / 3;}
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> pBuilder) {pBuilder.add(AGE);
     }
 }
 
