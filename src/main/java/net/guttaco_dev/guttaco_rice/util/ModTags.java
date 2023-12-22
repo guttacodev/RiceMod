fa04109396f048e799f4cc01acc3f8ddf9385a27
guttacodev <mail@guttaco.com>
2023-12-22 14:41:50 +0100

    changed a tag json to actually give rice a tag

--- src/main/java/net/guttaco_dev/guttaco_rice/util/ModTags.java
+++ src/main/java/net/guttaco_dev/guttaco_rice/util/ModTags.java
@@ -11,17 +11,6 @@ import net.minecraft.world.level.block.Block;
 
 
 public class ModTags {
-    public static class Blocks {
-        public static final  TagKey<Block> RICE_PLANT = forgeTag("crops/rice");
-        public static final TagKey<Block> tag(String name) {
-            return BlockTags.create(new ResourceLocation(RiceMod.MOD_ID, name));
-        }
-        private static TagKey<Block> forgeTag(String name) {
-            return BlockTags.create(new ResourceLocation("forge", name));
-        }
-    }
-
-
     public static class Items {
         public static final TagKey<Item> RICE_SEEDS = forgeTag("seeds/rice");
         public static final TagKey<Item> RICE = forgeTag("crops/rice"); //add option to use tags instead of item for recipe
